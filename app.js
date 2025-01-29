728b825c839b2d3551b24d45d07ac4f4e635e534
Nicolas Achuri <143991834+N1CKZ3B@users.noreply.github.com>
2025-01-29 19:56:20 +0000

    servidor creado

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+const tasks = [{id:1 , isCompleted:false , description: "Reunion 8 AM"},
+                {id:2, isCompleted:true , description : "Despertarse"}
+];
+
+app.get("/tasks", (req,res) => {
+    res.json(tasks);
+})
+
+app.get("/" , (req,res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log("Listening...");
+});

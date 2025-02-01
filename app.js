328d14058220f8aa36b5cb84ead9e91d55c631d6
Nicolas Achuri <143991834+N1CKZ3B@users.noreply.github.com>
2025-02-01 03:53:59 +0000

    Cambios introduccion

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.get("/" , (req,res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-
+app.get('/about', function (req, res) {
+    res.send('about');
+ });
+ 
 app.listen(port, () => {
     console.log("Listening...");
 });
